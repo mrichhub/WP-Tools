c0ff7f88871f18b3705c7969f2b16d69156ee415
Mike Richards <github@mikemrichards.com>
2013-03-14 12:00:46 -0400

    Added additional attributes to manipulate the toggles.

--- MikeRichards.WindowsPhone.Tools.WP8/Controls/Toggles/RoundedToggle.xaml.cs
+++ MikeRichards.WindowsPhone.Tools.WP8/Controls/Toggles/RoundedToggle.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MikeRichards.WindowsPhone.Tools.WP8.Controls.Toggles
@@ -13,6 +15,20 @@ namespace MikeRichards.WindowsPhone.Tools.WP8.Controls.Toggles
 		protected override UIElement ToggleSwitchElement
 		{
 			get { return SwitchEllipse; }
+		}
+
+		[Description("Gets or Sets the content label to the left of the switch")]
+		public string ContentLeft
+		{
+			get { return ContentLeftTextBlock.GetValue(TextBlock.TextProperty) as string; }
+			set { ContentLeftTextBlock.SetValue(TextBlock.TextProperty, value); }
+		}
+
+		[Description("Gets or Sets the content label to the right of the switch")]
+		public string ContentRight
+		{
+			get { return ContentRightTextBlock.GetValue(TextBlock.TextProperty) as string; }
+			set { ContentRightTextBlock.SetValue(TextBlock.TextProperty, value); }
 		}
 
 		private void Toggle_OnTap(object sender, GestureEventArgs e)
--- MikeRichards.WindowsPhone.Tools.WP8/Controls/Toggles/RoundedToggle3D.xaml.cs
+++ MikeRichards.WindowsPhone.Tools.WP8/Controls/Toggles/RoundedToggle3D.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace MikeRichards.WindowsPhone.Tools.WP8.Controls.Toggles
 {
@@ -14,6 +17,28 @@ namespace MikeRichards.WindowsPhone.Tools.WP8.Controls.Toggles
 		{
 			get { return SwitchEllipse; }
 		}
+
+		[Description("Gets or Sets the content label to the left of the switch")]
+		public string ContentLeft
+		{
+			get { return ContentLeftTextBlock.GetValue(TextBlock.TextProperty) as string; }
+			set { ContentLeftTextBlock.SetValue(TextBlock.TextProperty, value); }
+		}
+
+		[Description("Gets or Sets the content label to the right of the switch")]
+		public string ContentRight
+		{
+			get { return ContentRightTextBlock.GetValue(TextBlock.TextProperty) as string; }
+			set { ContentRightTextBlock.SetValue(TextBlock.TextProperty, value); }
+		}
+
+		[Description("Gets or Sets the Foreground Brush of the content labels")]
+		public Brush ContentForeground
+		{
+			get { return ContentLeftTextBlock.Foreground; }
+			set { ContentLeftTextBlock.Foreground = ContentRightTextBlock.Foreground = value; }
+		}
+
 
 		private void Toggle_OnTap(object sender, GestureEventArgs e)
 		{
